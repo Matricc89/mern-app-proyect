134087d099960bff4c6020158fc14a2002b97bda
MatiasRicci <matricc89@gmail.com>
2023-04-24 18:51:37 -0300

    go to6

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import { MONGODB_URI } from "./config.js";
+import { MONGO_URL } from "./config.js";
 
 
 export async function connectDB(){
     try {
-      const db =  await mongoose.connect(MONGODB_URI)
+      const db =  await mongoose.connect(MONGO_URL)
         console.log('connected to', db.connection.name)
     } catch (error) {
         console.error(error)
