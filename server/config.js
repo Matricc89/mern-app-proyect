d8ed87dd89b93c5ecc683c8eedfd46c726e59eb1
MatiasRicci <matricc89@gmail.com>
2023-04-24 17:04:46 -0300

    go to4

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const MONGODB_URI = process.env.MONGODE_URI || "mongodb://localhost/testdb"
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/testdb"
 export const PORT = process.env.PORT || 4000
