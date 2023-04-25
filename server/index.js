3f0f8e10bc14dbf278a98e560172a9d411efb27c
MatiasRicci <matricc89@gmail.com>
2023-04-24 21:55:22 -0300

    go to8

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import { connectDB } from './db.js'
 import app from './app.js'
+import { PORT } from './config.js'
 
 connectDB()
 
 
-app.listen(4000)
-console.log('server in running port', 4000)
+app.listen(PORT)
+console.log('server in running port', PORT)
